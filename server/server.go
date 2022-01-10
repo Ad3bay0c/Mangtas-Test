f9a311d5221f7afc2fccaaf4c597cbe68485f91d
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-10 22:49:53 +0100

    contains the logic for the handlers

--- server/server.go
+++ server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+type Server struct{}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) GetMostUsedWords(c *gin.Context) {
+	text := &struct {
+		Text string `json:"text"`
+	}{}
+	if err := c.ShouldBindJSON(text); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+        return
+	}
+	if strings.TrimSpace(text.Text) == "" {
+        c.JSON(400, gin.H{"error": "text is empty"})
+        return
+    }
+	c.JSON(200, gin.H{
+        "message": text.Text,
+    })
+}
