a4b5de6352df85793f352b465ab62bcee9421769
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-10 22:49:25 +0100

    Running server, setting up routers are done here

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "log"
+import (
+	"github.com/Ad3bay0c/golang-testing/server"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+const PORT = ":8080"
 
 func main () {
-	log.Printf("We are good")
+	router := gin.Default()
+	setUpRouter(router)
+
+	s := &http.Server{
+		Handler: router,
+		Addr: PORT,
+	}
+	log.Printf("Server started at http://localhost%v", PORT)
+	log.Fatal(s.ListenAndServe())
+}
+
+func setUpRouter(r *gin.Engine) {
+	s := server.NewServer()
+
+	r.POST("/", s.GetMostUsedWords)
 }
