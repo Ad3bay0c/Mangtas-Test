801997b7d8b4eea2341620a933679bbdbfe57fa6
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-11 15:04:42 +0100

    Reads input from file and returns 10 most used words

--- main2.go
+++ main2.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"github.com/Ad3bay0c/golang-testing/service"
+	"io/ioutil"
+)
+
+func main() {
+	file, err := ioutil.ReadFile("words.txt")
+	if err != nil {
+        panic(err)
+    }
+	result := service.GetMostUsedWords(string(file))
+
+	for idx, word := range result {
+		fmt.Printf("%d. %s\t%d\n", idx+1, word.Key, word.Value)
+	}
+}
